15e6d82487906df51f948cfd9e41ef99339b7598
Lucas LEFRANCQ <lucas.lefrancq@hotmail.fr>
2021-01-27 17:11:56 +0100

    need to add last list tests

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: llefranc <llefranc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 14:37:35 by hherin            #+#    #+#             */
-/*   Updated: 2021/01/27 15:18:59 by llefranc         ###   ########.fr       */
+/*   Updated: 2021/01/27 16:19:17 by llefranc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void	testVector(const char (&arrChar)[20], const int (&arrInt)[20], const std::s
 	
     std::cout.rdbuf(stdFile.rdbuf()); //redirect std::cout to ft::vector_result.txt
     ContainerConstruct<int, std::vector<int> >(arrInt, testNotConstVec<std::vector<int> >, testConstVec<const std::vector<int> >);
-    // ContainerConstruct<std::string, std::vector<std::string> >(arrString, testNotConstVec<std::vector<std::string> >, testConstVec<const std::vector<std::string> >);
+    ContainerConstruct<std::string, std::vector<std::string> >(arrString, testNotConstVec<std::vector<std::string> >, testConstVec<const std::vector<std::string> >);
     ContainerConstruct<char, std::vector<char> >(arrChar, testNotConstVec<std::vector<char> >, testConstVec<const std::vector<char> >);
 
 	nb = 0;
@@ -36,7 +36,7 @@ void	testVector(const char (&arrChar)[20], const int (&arrInt)[20], const std::s
 
 	std::cout.rdbuf(ftFile.rdbuf()); //redirect std::cout to std::vector_result.txt
     ContainerConstruct<int, ft::vector<int> >(arrInt, testNotConstVec<ft::vector<int> >, testConstVec<const ft::vector<int> >);
-    // ContainerConstruct<std::string, ft::vector<std::string> >(arrString, testNotConstVec<ft::vector<std::string> >, testConstVec<const ft::vector<std::string> >);
+    ContainerConstruct<std::string, ft::vector<std::string> >(arrString, testNotConstVec<ft::vector<std::string> >, testConstVec<const ft::vector<std::string> >);
     ContainerConstruct<char, ft::vector<char> >(arrChar, testNotConstVec<ft::vector<char> >, testConstVec<const ft::vector<char> >);
 
     std::cout.rdbuf(coutbuf); //reset to standard output again
@@ -55,7 +55,7 @@ void	testList(const char (&arrChar)[20], const int (&arrInt)[20], const std::str
 	
     std::cout.rdbuf(stdFile.rdbuf()); //redirect std::cout to ft::vector_result.txt
     ContainerConstruct<int, std::list<int> >(arrInt, testNotConstList<std::list<int> >, testConstList<const std::list<int> >);
-    // ContainerConstruct<std::string, std::list<std::string> >(arrString, testNotConstList<std::list<std::string> >, testConstList<const std::list<std::string> >);
+    ContainerConstruct<std::string, std::list<std::string> >(arrString, testNotConstList<std::list<std::string> >, testConstList<const std::list<std::string> >);
     ContainerConstruct<char, std::list<char> >(arrChar, testNotConstList<std::list<char> >, testConstList<const std::list<char> >);
 
 	nb = 0;
@@ -63,7 +63,7 @@ void	testList(const char (&arrChar)[20], const int (&arrInt)[20], const std::str
 
 	std::cout.rdbuf(ftFile.rdbuf()); //redirect std::cout to std::list_result.txt
     ContainerConstruct<int, ft::list<int> >(arrInt, testNotConstList<ft::list<int> >, testConstList<const ft::list<int> >);
-    // ContainerConstruct<std::string, ft::list<std::string> >(arrString, testNotConstList<ft::list<std::string> >, testConstList<const ft::list<std::string> >);
+    ContainerConstruct<std::string, ft::list<std::string> >(arrString, testNotConstList<ft::list<std::string> >, testConstList<const ft::list<std::string> >);
     ContainerConstruct<char, ft::list<char> >(arrChar, testNotConstList<ft::list<char> >, testConstList<const ft::list<char> >);
 
     std::cout.rdbuf(coutbuf); //reset to standard output again
